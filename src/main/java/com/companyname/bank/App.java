0846c8b15b0a92d9b6b2f596cf07f733eac1f53f
pratishkumar12 <92077033+pratishkumar12@users.noreply.github.com>
2022-07-21 22:24:26 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(new code "Hello World..new code modify" );
+        System.out.println("Hello World..new code modify" );
     }
 }
